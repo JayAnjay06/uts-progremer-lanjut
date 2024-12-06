4b8723e3ef8122f6f111e85c67ab6424e2935889
JayAnjay06 <jaylanicspr81@gmail.com>
2024-12-07 02:37:27 +0700

    menambahkan tampilan home screen

--- my-app/app/(root)/(tabs)/_layout.tsx
+++ my-app/app/(root)/(tabs)/_layout.tsx
@@ -17,15 +17,6 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="news"
-        options={{
-          title: "News",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="newspaper-outline" size={24} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
         name="history"
         options={{
           title: "History",
--- my-app/app/(root)/(tabs)/home.tsx
+++ my-app/app/(root)/(tabs)/home.tsx
@@ -1,13 +1,64 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,SafeAreaView, TouchableOpacity } from 'react-native'
 import React from 'react'
+import Octicons from '@expo/vector-icons/Octicons';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
-type Props = {}
-
-const HomeScreen = (props: Props) => {
+const HomeScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* Header */}
+      <View style={styles.header}>
+        <View style={styles.columHeader}>
+          <Text style={styles.textHeader}>SEKOLAH ID</Text>
+        </View>
+        <View>
+          <Text style={styles.hello}>Hallo</Text>
+        </View>
+        <View>
+          <Text style={styles.nama}>Nama Siswa</Text>
+        </View>
+      </View>
+
+      {/* Body */}
+      <View style={styles.body}>
+        <View style={styles.colum}>
+        <TouchableOpacity style={styles.columbody}>
+          <View style={styles.columStyle}>
+          <Octicons name="checklist" size={50} color="black" 
+          style={styles.icon}
+          />
+          </View>
+          <Text style={styles.textColum}>Absensi</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.columbody}>
+        <View style={styles.columStyle}>
+        <MaterialCommunityIcons name="microsoft-sharepoint" size={50} color="black"
+          style={styles.icon}
+          />
+          </View>
+          <Text style={styles.textColum}>Nilai</Text>
+        </TouchableOpacity>
+        </View>
+        <View style={styles.colum}>
+        <TouchableOpacity style={styles.columbody}>
+        <View style={styles.columStyle}>
+        <FontAwesome5 name="chalkboard-teacher" size={50} color="black"
+          style={styles.icon}
+          />
+          </View>
+          <Text style={styles.textColum}>Guru</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.columbody}>
+        <View style={styles.columStyle}>
+        <FontAwesome5 name="clipboard-list" size={50} color="black"          style={styles.icon}
+          />
+          </View>
+          <Text style={styles.textColum}>Jadwal</Text>
+        </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
@@ -16,7 +67,68 @@ export default HomeScreen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+  },
+  header:{
+    height:200,
+    width:"100%",
+    backgroundColor:"#375E97",
+    borderBottomLeftRadius:50,
+    borderBottomRightRadius:50
+  },
+  columHeader:{
+    height:50,
+    width:300,
+    marginTop:30,
+    marginHorizontal:'auto'
+  },
+  textHeader:{
+    fontSize:20,
+    fontWeight:"600",
+    textAlign:"center",
+    marginVertical:"auto",
+    color:"#FFFFFF"
+  },
+  hello:{
+    fontSize:15,
+    color:"#FFFFFF",
+    marginLeft:20
+  },
+  nama:{
+    fontSize:20,
+    color:"#FFFFFF",
+    marginLeft:20
+  },
+  body:{
+    width:"90%",
+    marginTop:20,
+    marginHorizontal:"auto",
+    gap:20
+
+  },
+  colum:{
+    marginHorizontal:"auto",
+    flexDirection:"row",
+    gap:20
+  },
+  columbody:{
+    width:150,
+    height:150,
+    borderRadius:10,
+    borderWidth:1
+  },
+  columStyle:{
+    height:50,
+    width:50,
+    marginVertical:"auto",
+    marginHorizontal:"auto"
+  },
+  icon:{
+    marginVertical:"auto",
+    marginHorizontal:"auto"
+  },
+  textColum:{
+    fontSize:20,
+    fontWeight:"700",
+    textAlign:"center",
   }
 })
